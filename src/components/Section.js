8d306438df60ce4dfcd7beb7d04876b39082a4b7
itsajay2195 <ajaykumar.95@yahoo.com>
2022-03-13 19:28:45 +0530

    completion of the model-s section

---
+++ src/components/Section.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+function Section() {
+  return (
+    <Wrap>
+        <ItemText>
+            <h1>Model S</h1>
+            <p>order online for touchless delivery</p>
+        </ItemText>
+
+        <Buttons>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+
+             <DownArrow src="images/down-arrow.svg"></DownArrow>
+        </Buttons>
+  
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    height: 100vh;
+    width: 100vw; 
+    background-size : cover;
+    background-position: center;
+    background-repeat:not-repeat;
+    background-image :url('/images/model-s.jpg');
+    display: flex;
+    flex-direction:column;
+    justify-content: space-between; //vertical
+    align-items: center; //horizontal
+  
+    
+`
+
+const ItemText = styled.div`
+   padding-top:15vh;
+   text-align:center; 
+`
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom:30px;
+  @media(max-width:768px){
+      flex-direction:column
+  }
+`
+
+const LeftButton = styled.div` 
+   background-color:rgba(23,26,32,0.8);
+   height: 40px;
+   width: 250px;
+   color:white;
+   display:flex;
+   justify-content:center;
+   align-items:center;
+   border-radius:100px;
+   opacity:0.85;
+   text-transform: uppercase;
+   font-size:12px;
+   cursor:pointer;
+   margin:8px
+`
+const RightButton = styled(LeftButton)`
+    background:white;
+    opacity:0.65;
+    color:black;
+`
+
+const DownArrow = styled.img` 
+   height:40px;
+   animation: animateDown 1s infinite;
+   overflow-x:hidden ;
+   `
+   const Buttons = styled.div` 
+    
+   `
+//vw full viEw width
+//
